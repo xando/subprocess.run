4cd176fdae7c65843a8b2a0ee0e817047af0045d
xando <sebastian.pawlus@gmail.com>
2013-11-03 13:04:19 +0100

    pipe tests

--- tests.py
+++ tests.py
@@ -1,8 +1,15 @@
 import sys
+import pytest
+import subprocess
 
+from functools import partial
 
 from run import run
 
+unix = pytest.mark.skipif(
+    not (sys.platform.startswith('linux') or sys.platform.startswith('darwin')),
+    reason="Unix test"
+)
 
 # dafault _commands
 class _commands:
@@ -45,15 +52,60 @@ def test_status():
     assert run('%s not_existing_directory' % _commands.rm).status != 0
 
 
-def test_pipe():
-    assert run(_commands.ls, _commands.more).status == 0
-
-
 def test_chain():
     command = run('ps aux', 'wc -l', 'wc -c')
 
     assert command.status == 0
     assert len(run('ps aux', 'wc -l', 'wc -c').chain) == 3
     assert [0, 0, 0] == [e.status for e in run('ps aux', 'wc -l', 'wc -c').chain]
-
     assert [1, 0] == [e.status for e in run('ps aux fail', 'wc -l').chain]
+
+
+Popen = partial(
+    subprocess.Popen,
+    universal_newlines=True,
+    stdin=subprocess.PIPE,
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+)
+
+@unix
+def test_popen_1():
+    command = Popen(['ls'])
+
+    assert command.communicate()[0] == run('ls').stdout
+
+
+@unix
+def test_popen_2():
+    command = Popen(['ls', '-la'])
+
+    assert command.communicate()[0] == run('ls -la').stdout
+
+
+@unix
+def test_popen_3():
+    command = Popen(['ls', '-la'])
+    command = Popen(['wc', '-c'], stdin=command.stdout)
+
+    assert command.communicate()[0] == run('ls -la', 'wc -c').stdout
+
+
+@unix
+def test_popen_4():
+
+    command = Popen(['cat', '/dev/urandom'])
+    command = Popen(['tr', '-dc', '"[:alpha:]"'], stdin=command.stdout)
+    command = Popen(["head", "-c", "10"], stdin=command.stdout)
+
+    assert len(command.communicate()[0]) == \
+        len(run("cat /dev/urandom", 'tr -dc "[:alpha:]"', "head -c 10").stdout)
+
+
+def test_stdin():
+    command = run(
+        "ls -la",
+        """python -c 'from run import run; print(run("wc -l", stdin=run.stdin).stdout)'"""
+    )
+
+    command.stdout.strip("\n") == run('ls -la', 'wc -l')
